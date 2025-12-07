29e8bb8413ea458b53fa44832cc303cd9e204111
guverz <guverz1994@mail.ru>
2025-12-07 23:20:06 +0800

    added fully added custom collisions, started working on interaction, currently doesnt work

---
+++ Assets/PlayerInteraction.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float playerReach = 3f;
+    Interactable currentInteractable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInteraction();
+        if (Input.GetKeyDown(KeyCode.F) && currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
+    }
+
+    void CheckInteraction()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if (Physics.Raycast(ray, out hit, playerReach))
+        {
+            if (hit.collider.tag == "Interactable")
+            {
+                Interactable newInteractable = hit.collider.GetComponent<Interactable>();
+
+                if (currentInteractable && newInteractable != currentInteractable)
+                {
+                    currentInteractable.DisableOutline();
+                }
+                if (newInteractable.enabled)
+                {
+                    SetNewCurrentInteractable(newInteractable);
+                }
+            }
+            else
+            {
+                DisableCurrentInteractable();
+            }
+        }
+        else
+        {
+            DisableCurrentInteractable();
+        }
+    }
+    void SetNewCurrentInteractable(Interactable newInteractable)
+    {
+        currentInteractable = newInteractable;
+        currentInteractable.EnableOutline();
+    }
+
+    void DisableCurrentInteractable()
+    {
+        if (currentInteractable)
+        {
+            currentInteractable.DisableOutline();
+            currentInteractable = null;
+        }
+    }
+}
